5feb126792ef61a0bccc3800af0ec16dd67eae78
Gustavo <88571393+GustaDNS@users.noreply.github.com>
2023-05-23 12:54:22 +0100

    js file add

---
+++ script.js
@@ -0,0 +1,15 @@
+function sim() {
+    alert("Só marcar então!");
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+function desvia(btn) {
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
